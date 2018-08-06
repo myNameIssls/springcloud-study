7f71fae64d84f5a36c154bd0ff19e8bf09d774a6
Tyrone <shanglishuai@sina.cn>
2018-08-06 15:47:34 +0800

    增加修改Ribbon默认负载均衡策略的方法

--- springcloud-service-consumer/src/main/java/cn/tyrone/springcloud/service/consumer/config/BeanConfig.java
+++ springcloud-service-consumer/src/main/java/cn/tyrone/springcloud/service/consumer/config/BeanConfig.java
@@ -5,13 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 @Configuration // 用于标记这个类属于配置类
 public class BeanConfig {
 
 	@Bean // @Bean相当于配置文件中<bean id = "restTemplate" class="org.springframework.web.client.RestTemplate" />
-	@LoadBalanced // 实现基于客户端的负载均衡
+	@LoadBalanced // 实现基于客户端的负载均衡,默认的轮询的策略
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public IRule irule() {
+		// 实现随机的负载均衡策略
+		return new RandomRule();
 	}
 	
 }
